f70c94cd14895eb6c745a9c4e96a6f92e3183389
swammj <swamyjose@unitec.edu>
2018-12-04 12:04:49 -0600

    change name

--- src/components/hero/heroComponent.js
+++ src/components/hero/heroComponent.js
@@ -6,7 +6,7 @@ class HeroComponent extends Component {
     return (
       <section className="main-section hero-header section--with-backdrop section--with-parallax">
         <h2 className="hero-header__subtitle">Hello there! I am</h2>
-        <h1 className="hero-header__title">Ramon Calix</h1>
+        <h1 className="hero-header__title">Swamy Jose Antunez</h1>
         <h2 className="hero-header__subtitle">Web developer, HN</h2>
     </section>
     );
